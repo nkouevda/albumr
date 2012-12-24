3cc5a4239fa7658e5c13e5b79ee01fd27ecdbe92
Nikita Kouevda <nkouevda@gmail.com>
2012-12-24 01:35:57 -0800

    Add help option; use getopts instead of getopt; use lowercase vars.

--- imgur.sh
+++ imgur.sh
@@ -1,85 +1,109 @@
 #!/bin/bash
 
 # Nikita Kouevda
-# 2012/12/23
+# 2012/12/24
 
 # Store script name and directory
-SCRIPT_NAME="${0##*/}"
-SCRIPT_DIR="${0%/*}"
+script_name="${0##*/}"
+script_dir="${0%/*}"
 
-# Parse the given arguments
-PARSED_ARGS=$(getopt v $*)
+# Prints usage
+function usage () {
+    echo "usage: $script_name [-h] [-v] [--] [album ...]"
+}
 
-# Print usage and exit if parsing error
-if [[ $? != 0 ]]; then
-    echo "Usage: $SCRIPT_NAME [-v] [album ...]"
-    exit 1
-fi
+# Prints help message
+function help_message () {
+    usage
+    cat <<EOF
+options:
+    -h      Show this help message.
+    -v      Enable verbose output.
+EOF
+}
 
-# Set arguments to parsed arguments
-set -- $PARSED_ARGS
+# Returns 0 iff verbose output is enabled
+function verbose () {
+    [[ -n "$verbose_option" ]]
+    return $?
+}
 
 # Iterate over options
-for ARG in "$@"; do
-    case "$ARG" in
-        # Enable verbose output
-        -v)
-            VERBOSE=1
-            shift
+while getopts "hv" option "$@"; do
+    case "$option" in
+        # Parsing error; print usage and exit
+        \?)
+            usage
+            exit 1
             ;;
-        # End options
-        --)
-            shift
-            break
+        # Print help message and exit
+        h)
+            help_message
+            exit 0
+            ;;
+        # Enable verbose output
+        v)
+            verbose_option=0
             ;;
     esac
 done
 
+# Set arguments to remaining positional arguments
+set -- ${@:$OPTIND}
+
+# Print usage and exit if no positional arguments given
+if [[ -z "$*" ]]; then
+    usage
+    exit 0
+fi
+
 # Iterate over the given albums
-for ALBUM in "$@"; do
+for album in "$@"; do
     # Print the album being processed
-    [[ "$VERBOSE" ]] && echo "Processing album: $ALBUM"
+    verbose && echo "processing album: $album"
 
     # Determine the album hash
-    if [[ -n "$(curl -sI "http://imgur.com/a/$ALBUM" | head -n 1 | grep 200)" ]]; then
-        ALBUM_HASH=${ALBUM:0:5}
-    elif [[ -n "$(curl -sI "$ALBUM" | head -n 1 | grep 200)" ]]; then
-        ALBUM_HASH=$(echo "$ALBUM" | perl -ne 'm/imgur.com\/a\/(\w{5})/; print $1')
+    if [[ -n "$(curl -sI -- "http://imgur.com/a/$album" | head -n 1 | grep 200)" ]]; then
+        album_hash=${album:0:5}
+    elif [[ -n "$(curl -sI -- "$album" | head -n 1 | grep 200)" ]]; then
+        album_hash=$(echo "$album" | perl -ne 'm/imgur.com\/a\/(\w{5})/; print $1')
     else
-        echo "Error: could not read album: $ALBUM" >&2
+        echo "error: could not read album: $album" >&2
         continue
     fi
 
     # Determine the album URL based on the hash
-    ALBUM_URL="http://imgur.com/a/$ALBUM_HASH"
+    album_url="http://imgur.com/a/$album_hash"
 
     # Print the album hash and URL
-    [[ "$VERBOSE" ]] && echo "Album hash: $ALBUM_HASH"
-    [[ "$VERBOSE" ]] && echo "Album URL: $ALBUM_URL"
+    verbose && echo "album hash: $album_hash"
+    verbose && echo "album URL: $album_url"
 
     # Use the album hash to make the directory if necessary
-    if [[ ! -e "$ALBUM_HASH" ]]; then
-        [[ "$VERBOSE" ]] && echo "Making directory: $ALBUM_HASH"
-        mkdir "$ALBUM_HASH"
-    elif [[ ! -d "$ALBUM_HASH" ]]; then
-        echo "Error: not a directory: $ALBUM_HASH" >&2
+    if [[ ! -e "$album_hash" ]]; then
+        verbose && echo "making directory: $album_hash"
+        mkdir "$album_hash"
+    elif [[ ! -d "$album_hash" ]]; then
+        echo "error: not a directory: $album_hash" >&2
         continue
     fi
 
     # Extract the image hashes and extensions
-    IMAGES=$(curl -s "$ALBUM_URL" | tr '}' '\n' | perl -ne 'print "$1$2\n" if m/"hash":"(.{5})".+"ext":"(\..{3,4})"/')
+    images=$(curl -s "$album_url" | tr '}' '\n' | perl -ne 'print "$1$2\n" if m/"hash":"(.{5})".+"ext":"(\..{3,4})"/')
 
     # Download and store the images concurrently
-    for IMAGE in $IMAGES; do
+    for image in $images; do
         (
-            curl -so "$ALBUM_HASH/$IMAGE" "http://i.imgur.com/$IMAGE"
-            ERROR_CODE=$?
+            # Download the image and save it, storing the return code of curl
+            verbose && echo "downloading image: $image"
+            curl -so "$album_hash/$image" "http://i.imgur.com/$image"
+            return_code=$?
 
-            if [[ "$VERBOSE" ]]; then
-                if [[ $ERROR_CODE == 0 ]]; then
-                    echo "Saved image: $IMAGE"
+            if verbose; then
+                if [[ $return_code -eq 0 ]]; then
+                    echo "saved image: $image"
                 else
-                    echo "Error: could not download image: $IMAGE" >&2
+                    echo "error: could not download image: $image" >&2
                 fi
             fi
         ) &
@@ -90,4 +114,4 @@ done
 wait
 
 # Indicate completion
-[[ "$VERBOSE" ]] && echo "Done"
+verbose && echo "done"
