cfcf2627a21e0d7f5fda4b82e5e27747c515145e
Nikita Kouevda <nkouevda@gmail.com>
2012-12-24 22:29:57 -0800

    Fix downloads of images with trailing chars after ext.

--- imgur.sh
+++ imgur.sh
@@ -9,7 +9,7 @@ script_dir="${0%/*}"
 
 # Prints usage
 function usage () {
-    echo "usage: $script_name [-h] [-v] [--] [album ...]"
+    echo "usage: $script_name [-h] [-v] [--] album ..."
 }
 
 # Prints help message
@@ -31,9 +31,9 @@ function verbose () {
 # Iterate over options
 while getopts "hv" option "$@"; do
     case "$option" in
-        # Parsing error; print usage and exit
+        # Parsing error; print usage to stderr and exit
         \?)
-            usage
+            usage >&2
             exit 1
             ;;
         # Print help message and exit
@@ -48,7 +48,7 @@ while getopts "hv" option "$@"; do
     esac
 done
 
-# Set arguments to remaining positional arguments
+# Set shell positional arguments to the remaining arguments
 set -- ${@:$OPTIND}
 
 # Print usage and exit if no positional arguments given
@@ -57,16 +57,17 @@ if [[ -z "$*" ]]; then
     exit 0
 fi
 
-# Iterate over the given albums
+# Iterate over the given albums concurrently
 for album in "$@"; do
+    (
     # Print the album being processed
-    verbose && echo "processing album: $album"
+    verbose && echo "starting album: $album"
 
     # Determine the album hash
     if [[ -n "$(curl -sI -- "http://imgur.com/a/$album" | head -n 1 | grep 200)" ]]; then
         album_hash=${album:0:5}
     elif [[ -n "$(curl -sI -- "$album" | head -n 1 | grep 200)" ]]; then
-        album_hash=$(echo "$album" | perl -ne 'm/imgur.com\/a\/(\w{5})/; print $1')
+        album_hash=$(echo "$album" | perl -ne 'm/imgur.com\/a\/([A-Za-z0-9]{5})/; print $1')
     else
         echo "error: could not read album: $album" >&2
         continue
@@ -89,28 +90,34 @@ for album in "$@"; do
     fi
 
     # Extract the image hashes and extensions
-    images=$(curl -s "$album_url" | tr '}' '\n' | perl -ne 'print "$1$2\n" if m/"hash":"(.{5})".+"ext":"(\..{3,4})"/')
+    images=$(curl -s "$album_url" | tr '}' '\n' | perl -ne 'print "$1$2\n" if m/"hash":"([A-Za-z0-9]{5})".+?"ext":"(.+?)"/')
 
     # Download and store the images concurrently
     for image in $images; do
         (
-            # Download the image and save it, storing the return code of curl
-            verbose && echo "downloading image: $image"
-            curl -so "$album_hash/$image" "http://i.imgur.com/$image"
-            return_code=$?
+        # Strip any trailing characters for the local filename
+        filename=$(echo $image | perl -pe 's/([A-Za-z0-9]\.[A-Za-z]{3,4}).*/$1/g')
 
-            if verbose; then
-                if [[ $return_code -eq 0 ]]; then
-                    echo "saved image: $image"
-                else
-                    echo "error: could not download image: $image" >&2
-                fi
-            fi
+        # Download the image and save it, storing the return code of curl
+        verbose && echo "downloading: http://i.imgur.com/$image"
+        curl -so "$album_hash/$filename" "http://i.imgur.com/$image"
+        return_code=$?
+
+        # Print success if verbose output enabled; print error regardless
+        [[ $return_code -eq 0 && verbose ]] && echo "saved: $album_hash/$filename"
+        [[ $return_code -ne 0 ]] && echo "error: download or save failed: $image" >&2
         ) &
     done
+
+    # Wait for all images to download
+    wait
+
+    # Indicate album completion
+    verbose && echo "finished album: $album"
+    ) &
 done
 
-# Wait for all background jobs to finish
+# Wait for all albums to finish
 wait
 
 # Indicate completion
