6a2a725e9239b9f640dae3f619340c1ee22fd10c
Nikita Kouevda <nkouevda@gmail.com>
2013-05-31 18:16:15 -0700

    Add title and verbose options.

--- albumr.py
+++ albumr.py
@@ -10,25 +10,26 @@ import re
 from urllib.request import urlopen
 
 
-def save_image(image_tuple):
-    # Unpack the album, number, and image
-    album, number, image = image_tuple
+def save_image(directory_number_image_verbose):
+    directory, number, image, verbose = directory_number_image_verbose
 
-    # Save each file 'http://i.imgur.com/[image]' to '[album]/[number]-[image]'
+    # Save 'http://i.imgur.com/$image' to '$directory/$number-$image'
     url = 'http://i.imgur.com/' + image
-    local_path = '{0}/{1}-{2}'.format(album, number, image)
+    path = '{0}/{1}-{2}'.format(directory, number, image)
 
-    if os.path.exists(local_path):
-        print('error: file exists: ' + local_path)
+    if os.path.exists(path):
+        print('error: file exists: ' + path)
     else:
         try:
             # Read and write the binary image data
             with urlopen(url) as in_url:
-                with open(local_path, 'wb') as out_file:
-                    print('saving file: ' + local_path)
+                with open(path, 'wb') as out_file:
+                    if verbose:
+                        print('saving file: ' + path)
+
                     out_file.write(in_url.read())
         except:
-            print('error: could not save file: ' + local_path)
+            print('error: could not save file: ' + path)
 
 
 def main():
@@ -36,8 +37,15 @@ def main():
 
     # Require at least one album name
     parser.add_argument(
-        'albums', nargs='+', type=str, help='an album hash or URL',
-        metavar='album')
+        'albums', nargs='+', type=str, metavar='album',
+        help='an album hash or URL')
+
+    parser.add_argument(
+        '-t', '--title', action='store_true',
+        help='append album title to directory name')
+
+    parser.add_argument(
+        '-v', '--verbose', action='store_true', help='verbose output')
 
     # Parse the given command-line arguments
     args = parser.parse_args()
@@ -46,29 +54,37 @@ def main():
 
     for album in args.albums:
         try:
-            # Extract the album hash
-            album = re.match(
+            # Determine the album hash
+            directory = album = re.match(
                 r'(?:.*/)?([A-Za-z0-9]{5}?)(?:#.*)?$', album).group(1)
 
-            # Read and decode the content
+            # Read and decode the album page's content
             with urlopen('http://imgur.com/a/' + album) as in_url:
                 content = in_url.read().decode()
         except:
             print('error: could not read album: ' + album)
             continue
 
+        if args.title:
+            # Extract the title, sanitize it, and append it to the directory
+            title = re.search(r'data-title="(.*?)"', content).group(1)
+            title = re.sub(r'(?:[^ -~]|[/:])+', ' ', title)
+            directory += '-[' + title + ']'
+
         # Find all pairs of image hashes and extensions in the content
         pairs = re.finditer(
             r'"hash":"([A-Za-z0-9]{5,7})".+?"ext":"(.+?)"', content)
 
-        # Create the directory for this album if it does not exist
-        if not os.path.exists(album):
-            print('making directory: ' + album)
-            os.makedirs(album)
+        if not os.path.exists(directory):
+            if args.verbose:
+                print('making directory: ' + directory)
 
-        # Add each image to the set, along with its album and number
+            os.makedirs(directory)
+
+        # Add each image and its relevant information to the set
         for number, pair in enumerate(pairs):
-            images.add((album, number + 1, pair.group(1) + pair.group(2)))
+            image = pair.group(1) + pair.group(2)
+            images.add((directory, number + 1, image, args.verbose))
 
     # Use a process pool to simultaneously save images
     pool = multiprocessing.Pool()
