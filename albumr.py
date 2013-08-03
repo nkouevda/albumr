6af0bdb2bf2a7411b73b1c4e57552d3af8ebc41a
Nikita Kouevda <nkouevda@gmail.com>
2013-08-03 13:41:52 -0700

    Separate argument parsing from saving; clean up.

--- albumr.py
+++ albumr.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # Nikita Kouevda
-# 2013/06/10
+# 2013/08/03
 
 import argparse
 import html.parser
@@ -14,7 +14,7 @@ from urllib.request import urlopen
 def save_image(directory_number_image_verbose):
     directory, number, image, verbose = directory_number_image_verbose
 
-    # Save 'http://i.imgur.com/$image' to '$directory/$number-$image'
+    # Save 'http://i.imgur.com/`image`' to '`directory`/`number`-`image`'
     url = 'http://i.imgur.com/' + image
     path = '{0}/{1}-{2}'.format(directory, number, image)
 
@@ -22,7 +22,6 @@ def save_image(directory_number_image_verbose):
         print('error: file exists: ' + path)
     else:
         try:
-            # Read and write the binary image data
             with urlopen(url) as in_url:
                 with open(path, 'wb') as out_file:
                     if verbose:
@@ -33,66 +32,66 @@ def save_image(directory_number_image_verbose):
             print('error: could not save file: ' + path)
 
 
-def main():
-    parser = argparse.ArgumentParser(description="Download Imgur albums")
-
-    # Require at least one album name
-    parser.add_argument(
-        'albums', nargs='+', type=str, metavar='album',
-        help='an album hash or URL')
-
-    parser.add_argument(
-        '-t', '--title', action='store_true',
-        help='append album title to directory name')
-
-    parser.add_argument(
-        '-v', '--verbose', action='store_true', help='verbose output')
-
-    # Parse the given command-line arguments
-    args = parser.parse_args()
+def save_albums(albums, include_title=False, verbose=False):
+    html_parser = html.parser.HTMLParser()
+    re_album_hash = re.compile(r'(?:.*/)?([A-Za-z0-9]{5})(?:[/#].*)?$')
+    re_image = re.compile(r'"hash":"([A-Za-z0-9]{5,7})".+?"ext":"(.+?)"')
+    re_title = re.compile(r'data-title="(.*?)"')
+    re_title_sanitize = re.compile(r'(?:[^ -~]|[/:])+')
 
     images = set()
 
-    for album in args.albums:
-        try:
-            # Determine the album hash
-            directory = album = re.match(
-                r'(?:.*/)?([A-Za-z0-9]{5})(?:[/#].*)?$', album).group(1)
+    for album in albums:
+        directory = album_hash = re_album_hash.match(album).group(1)
 
-            # Read and decode the album page's content
-            with urlopen('http://imgur.com/a/' + album) as in_url:
+        try:
+            with urlopen('http://imgur.com/a/' + album_hash) as in_url:
                 content = in_url.read().decode()
         except:
-            print('error: could not read album: ' + album)
+            print('error: could not read album: ' + album_hash)
             continue
 
-        if args.title:
-            # Extract the title, sanitize it, and append it to the directory
-            title = re.search(r'data-title="(.*?)"', content).group(1)
-            title = html.parser.HTMLParser().unescape(title)
-            title = re.sub(r'(?:[^ -~]|[/:])+', ' ', title)
-            directory += '-[' + title + ']'
-
-        # Find all pairs of image hashes and extensions in the content
-        pairs = re.finditer(
-            r'"hash":"([A-Za-z0-9]{5,7})".+?"ext":"(.+?)"', content)
+        if include_title:
+            # Extract the title, sanitize it, and include it in the directory
+            title_raw = re_title.search(content).group(1)
+            title_parsed = html_parser.unescape(title_raw)
+            title_sanitized = re_title_sanitize.sub(' ', title_parsed)
+            directory += '-[' + title_sanitized + ']'
 
         if not os.path.exists(directory):
-            if args.verbose:
+            if verbose:
                 print('making directory: ' + directory)
 
             os.makedirs(directory)
 
         # Add each image and its relevant information to the set
-        for number, pair in enumerate(pairs):
+        for number, pair in enumerate(re_image.finditer(content)):
             image = pair.group(1) + pair.group(2)
-            images.add((directory, number + 1, image, args.verbose))
+            images.add((directory, number + 1, image, verbose))
 
     # Use a process pool to simultaneously save images
     pool = multiprocessing.Pool()
     pool.imap_unordered(save_image, images)
     pool.close()
     pool.join()
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Download Imgur albums')
+
+    # Require at least one album
+    parser.add_argument('albums', nargs='+', type=str, metavar='album',
+                        help='an album hash or URL')
+
+    parser.add_argument('-t', '--title', action='store_true',
+                        help='append album title to directory name')
+
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='verbose output')
+
+    args = parser.parse_args()
+
+    save_albums(args.albums, include_title=args.title, verbose=args.verbose)
 
 
 if __name__ == '__main__':
